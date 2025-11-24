0aed56687ac1eaacfc60b868a030ded82b88ce31
o-Anna-o <annashulzhinskaya26@gmail.com>
2025-11-24 22:59:30 +0300

    выложено на Github pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 // src/components/Navbar.tsx
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const CART_ICON_BASE64 =
@@ -66,7 +66,20 @@ export default function Navbar() {
       </div>
 
       <Link to="/ships">
-        <img src="/resources/img/home-img.svg" alt="home" style={{ height: 40 }} />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="40"
+                height="40"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
+                <path d="M3 10a2 2 0 0 1 .709-1.528l7-6a2 2 0 0 1 2.582 0l7 6A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+              </svg>
       </Link>
     </div>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,28 @@
 // src/main.tsx
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
-import App from './App'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import '../resources/index_style.css'
-import '../resources/request_ship_style.css'
-import '../resources/ship_style.css'
+import './resources/index_style.css'
+import './resources/request_ship_style.css'
+import './resources/ship_style.css'
 
 import { Provider } from 'react-redux'
 import { store } from './store'
 
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import routes from './App'
+import React from 'react'
+
+const router = createBrowserRouter(routes, {
+  basename: '/loading-time-frontend/'
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  
+  <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
+  </React.StrictMode>
 )
